644d15119ecde170bc7b5e2086f4781140a1cced
sabinopa <priasabino@gmail.com>
2024-08-23 19:25:38 -0300

    refactor to improve readability

--- customer_success_balancing.rb
+++ customer_success_balancing.rb
@@ -2,68 +2,65 @@ require 'minitest/autorun'
 require 'timeout'
 
 class CustomerSuccessBalancing
+
   def initialize(customer_success, customers, away_customer_success)
     @customer_success = customer_success
     @customers = customers
     @away_customer_success = away_customer_success
   end
 
-  # Returns the ID of the customer success with most customers
   def execute
-    cs_count = allocate_cs_to_customers
-    identify_top_customer_success(cs_count)
+    allocated_customers = allocate_customers_to_cs
+    identify_top_customer_success(allocated_customers)
   end
 
   private
+
+  def allocate_customers_to_cs
+    customer_index = 0
+    customer_count = sorted_customer_list.size
+    allocated_customers = Hash.new(0)
+
+    sorted_customer_success_list.each do |current_cs|
+      while more_customers_to_allocate?(customer_index, customer_count, current_cs)
+        allocated_customers[current_cs[:id]] += 1
+        customer_index += 1
+      end
+    end
+
+    allocated_customers
+  end
+
+  def sorted_customer_list
+    @sorted_customer_list ||= sort_by_score(@customers)
+  end
+
+  def sorted_customer_success_list
+    @sorted_customer_success_list ||= sort_by_score(available_customer_success)
+  end
+
+  def sort_by_score(list)
+    list.sort_by! { |entity| entity[:score] }
+  end
 
   def available_customer_success
     @customer_success.reject { |cs| @away_customer_success.include?(cs[:id]) }
   end
 
-  def sorted_customer_success
-    available_customer_success.sort_by! { |cs| cs[:score] }
+  def more_customers_to_allocate?(customer_index, customer_count, current_cs)
+    customer_index < customer_count && sorted_customer_list[customer_index][:score] <= current_cs[:score]
   end
 
-  def sorted_customers
-    @customers.sort_by! { |customer| customer[:score] }
+  def identify_top_customer_success(allocated_customers)
+    return 0 if allocated_customers.empty?
+
+    max_customers_cs = allocated_customers.max_by { |_, count| count }
+    return 0 if top_cs_draw?(allocated_customers, max_customers_cs)
+
+    max_customers_cs.first
   end
 
-  def allocate_cs_to_customers
-    customer_success_list = sorted_customer_success
-    customer_list = sorted_customers
-    customer_count = Hash.new(0)
-    process_customers(customer_success_list, customer_list, customer_count)
-    customer_count
-  end
-
-  def process_customers(customer_success_list, customer_list, customer_count)
-    cs_index = 0
-    customer_list.each do |customer|
-      cs_index = find_suitable_cs(customer_success_list, cs_index, customer)
-      break if cs_index == customer_success_list.size
-
-      increment_customer_count(customer_count, customer_success_list, cs_index)
-    end
-  end
-
-  def find_suitable_cs(customer_success_list, cs_index, customer)
-    while cs_index < customer_success_list.size && customer_success_list[cs_index][:score] < customer[:score]
-      cs_index += 1
-    end
-    cs_index
-  end
-
-  def increment_customer_count(customer_count, customer_success_list, cs_index)
-    cs_id = customer_success_list[cs_index][:id]
-    customer_count[cs_id] += 1
-  end
-
-  def identify_top_customer_success(customer_count)
-    return 0 if customer_count.empty?
-
-    max_customers_cs = customer_count.max_by { |_, count| count }
-    return 0 if customer_count.values.count(max_customers_cs[1]) > 1
-
-    max_customers_cs[0]
+  def top_cs_draw?(allocated_customers, max_customers_cs)
+    allocated_customers.values.count(max_customers_cs[1]) > 1
   end
 end
